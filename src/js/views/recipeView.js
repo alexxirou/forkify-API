918c85020e028929f25f6cc5e8712498b91777d4
Alexandros Xirouchakis <alex.xirou@hotmail.com>
2023-07-31 20:13:33 +0300

    Update recipeView.js remove fractional library

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,4 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import { mark } from 'regenerator-runtime';
 import View from './View.js';
 
@@ -104,7 +103,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ingredient.quantity}</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ingredient.unit || ''}</span>
               ${ingredient.description}
