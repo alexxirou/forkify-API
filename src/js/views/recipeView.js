84e1c4ba8a8edb35f4355bb49143ee7a43d484be
Alexandros Xirouchakis <alex.xirou@hotmail.com>
2023-07-31 17:37:23 +0300

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -104,7 +104,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ingredient.quantity ? new Fraction.Fraction(ingredient.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ingredient.unit || ''}</span>
               ${ingredient.description}
